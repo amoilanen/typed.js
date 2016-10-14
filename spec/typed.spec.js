63585ae10a6289df1f97a32877a168c4f7ea173c
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-10-14 21:16:32 +0300

    Add 'number' type checking

--- spec/typed.spec.js
+++ spec/typed.spec.js
@@ -1,8 +1,24 @@
-import { type } from './lib/typed'
+import { type, number } from './lib/typed'
 
-describe('typed', () => {
+describe('type', () => {
 
-  it('type should be defined', () => {
-    expect(type).not.toBeUndefined();
+  describe('number', () => {
+
+    it('returns number argument', () => {
+      expect(number(5)).toEqual(5);
+    });
+
+    it('throws exception if not a number', () => {
+      expect(() => number('abc')).toThrow(new Error('Type violation: abc is not a number'));
+    });
   });
+
+  //TODO: Primitive types: bool, number, string
+  //TODO: Type array
+  //TODO: Type object
+  //TODO: Define type as a shape, inspiration from React properties
+
+  //TODO: Controlling/specifying return type of a function?
+  //TODO: Integration with React?
+  //TODO: Option to switch off checks/ignore errors?
 });
